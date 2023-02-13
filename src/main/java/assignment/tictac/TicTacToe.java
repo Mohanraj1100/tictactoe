c2da6a4083b758acf95a6d4ead43a26b04592484
Mohanraj1100 <mohanraj.anbuselvam@tringapps.com>
2023-02-13 18:06:31 +0530

    initial

---
+++ src/main/java/assignment/tictac/TicTacToe.java
@@ -0,0 +1,259 @@
+package assignment.tictac;
+
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class TicTacToe 
+{
+	static public int size;
+    public static void main( String[] args )
+    {
+    	Logger l = Logger.getLogger("hi");
+    	Scanner sc = new Scanner(System.in);
+    	PrintStream ps = new PrintStream(new FileOutputStream(FileDescriptor.out));
+    	XoBoard xb = new XoBoard();
+    	//Playing pl = new Playing();
+    	int position;
+    	int s=xb.size;
+    	int i=0;
+    	xb.printArray();
+    	l.info("Player 1  x");
+    	l.info("Player 2  o");
+    	//char move = sc.next().charAt(0);
+    	while(i<s*s)
+    	{
+    		if(i%2==0)
+    		{
+    			l.info("First Player Enter the position");
+    			position = sc.nextInt();
+    			int val = xb.insert(position,'x');
+    			if(val == 1) {
+    				i--;
+    			}
+    			xb.printArray();
+    			xb.checkXoLeadDiagonal();
+    			xb.checkXoDiagonal();
+    			xb.chechColumn();
+    			xb.chechRow();
+    		}
+    		else
+    		{
+    			l.info("Second Player Enter the position");
+    			position = sc.nextInt();
+    			int val=xb.insert(position,'o');
+    			if(val == 1) {
+    				i--;
+    			}
+    			else
+    			{
+    			xb.printArray();
+    			xb.checkXoLeadDiagonal();
+    			xb.checkXoDiagonal();
+    			xb.chechColumn();
+    			xb.chechRow();
+    			}
+    		}
+    		i++;
+    		if(i==9)
+    		{
+    			l.info("Match has been Drawn");
+    		}
+    		
+    	}   	
+    }
+
+	
+}
+class XoBoard
+{
+	static PrintStream ps = new PrintStream(new FileOutputStream(FileDescriptor.out));
+	Logger l = Logger.getLogger("hi");
+	Scanner is = new Scanner(System.in);
+	char xo[][];
+	int size;
+	XoBoard()
+	{
+		ps.print("Enter the size of game");
+		this.size = is.nextInt();
+		xo=new char[size][size];
+		for(int i=0;i<size;i++)
+		{
+			for(int j=0;j<size;j++)
+				{
+					xo[i][j]=' ';
+				}
+		}
+	}
+	void printArray()
+	{
+	for(int i=0;i<size;i++)
+	{
+		ps.print('|');
+		for(int j=0;j<size;j++)
+			{
+				ps.print(xo[i][j]);
+				ps.print('|');
+			}
+		ps.println();
+	}
+	}
+	public int insert(int posi,char m)
+	{
+		System.out.println(posi);
+		int row = (posi-1)/size;
+		int col = (posi-1)%size;
+		if(xo[row][col] == ' ' )
+		{
+			xo[row][col]=m;
+		}
+		else
+		{
+			l.info("Invalid Choice");
+			return 1 ;
+		}
+		return 0;
+	}
+	
+	public void checkXoLeadDiagonal()
+	{
+		int player1=0;
+		int player2=0;
+		
+		for(int i=0;i<size;i++)
+		{
+			if(xo[i][i]== 'x' )
+			{
+				player1++;
+			}
+			else if(xo[i][i]=='o')
+			{
+				player2++;
+			}
+		}	
+		if(player1 == size)
+		{
+			l.info("Player 1 wins");
+			printArray();
+			System.exit(0);
+		}
+		if(player2 == size)
+		{
+			l.info("Player 2 wins");
+			printArray();
+			System.exit(0);
+		}
+	}
+	public void checkXoDiagonal()
+	{
+		
+		for(int i=0;i<size;i++)
+		{
+			int player1=0;
+			int player2=0;
+			for(int j=0;j<size;j++)
+			{
+				if((i+j) == size-1)
+				{
+					if(xo[i][j]== 'x' )
+					{
+						player1++;
+					}
+					else if(xo[i][j]=='o')
+					{
+						player2++;
+					}
+				}
+			}
+			if(player1 == size)
+			{
+				l.info("Player 1 wins");
+				printArray();
+				System.exit(0);
+			}
+			if(player2 == size)
+			{
+				l.info("Player 2 wins");
+				printArray();
+				System.exit(0);
+			}
+		}
+		
+		
+	}
+	public void chechColumn()
+	{
+		
+		for(int i=0;i<size;i++)
+		{
+			int player1=0;
+			int player2=0;
+			for(int j=0;j<size;j++)
+			{
+				if(xo[j][i]=='x')
+				{
+					player1++;
+				}
+				else if(xo[j][i]=='o')
+				{
+					player2++;
+				}
+			}
+			if(player1 == size)
+			{
+				l.info("Player 1 wins");
+				printArray();
+				System.exit(0);
+			}
+			if(player2 == size)
+			{
+				l.info("Player 2 wins");
+				printArray();
+				System.exit(0);
+			}
+		}
+		
+		
+	}
+	public void chechRow()
+	{
+		for(int i=0;i<size;i++)
+		{
+			int player1=0;
+			int player2=0;
+			
+			for(int j=0;j<size;j++)
+			{
+				if(xo[i][j]=='x')
+				{
+					player1++;
+				}
+				else if(xo[i][j]=='o')
+				{
+					player2++;
+				}
+			}
+			if(player1 == size)
+			{
+				l.info("Player 1 wins");
+				printArray();
+				System.exit(0);
+			}
+			if(player2 == size)
+			{
+				l.info("Player 2 wins");
+				printArray();
+				System.exit(0);
+			}
+		}
+		
+		
+	}
+	public void draw()
+	{
+		l.info("Match has been Drawn");
+	}
+	
+}
---
+++ src/test/java/assignment/tictac/AppTest.java
@@ -0,0 +1,20 @@
+package assignment.tictac;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
